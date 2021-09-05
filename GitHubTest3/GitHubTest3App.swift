70a0d28172d30d9cac5b50eea7836e281a83a35e
양석모 <nepastel78@gmail.com>
2021-09-05 13:13:28 +0900

    Initial Commit

---
+++ GitHubTest3/GitHubTest3App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTest3App.swift
+//  GitHubTest3
+//
+//  Created by 양석모 on 2021/09/05.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTest3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
